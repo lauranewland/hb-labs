444019bed10af6d5f0ced04236e2ac56c61043fb
Laura Newland <laurasmora101@gmail.com>
2020-10-22 16:53:15 +0000

    Created Class User
    Co-authored-by: kayteecannon <kaytee.cannon@me.com>

--- ratings-v2/model.py
+++ ratings-v2/model.py
@@ -18,6 +18,22 @@ def connect_to_db(flask_app, db_uri='postgresql:///ratings', echo=True):
 
     print('Connected to the db!')
 
+class User(db.Model):
+    """A user."""
+
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer,
+                        autoincrement=True,
+                        primary_key=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+
+
+
 
 if __name__ == '__main__':
     from server import app
