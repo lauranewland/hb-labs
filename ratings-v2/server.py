4bf7d239884d9cacaf387c31c77c6bbeb1e66ddf
Laura Newland <laurasmora101@gmail.com>
2020-10-23 20:24:34 +0000

    Set Co-authored-by: kayteecannon <kaytee.cannon@me.com>

--- ratings-v2/server.py
+++ ratings-v2/server.py
@@ -18,6 +18,13 @@ def homepage():
 
     return render_template('homepage.html')
 
+@app.route('/movies')
+def all_movies():
+    """View all movies"""
+
+    movies = crud.get_movies()
+
+    return  render_template('all_movies.html', movies=movies)
 
 if __name__ == '__main__':
     connect_to_db(app)
