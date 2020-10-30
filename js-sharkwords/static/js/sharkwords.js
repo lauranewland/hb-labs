3c01346ed2a8a29389a75be08369ff133adc4315
Laura Newland <laurasmora101@gmail.com>
2020-10-30 01:02:40 +0000

    Completed createDivsForChars and generateLetterButtons

--- js-sharkwords/static/js/sharkwords.js
+++ js-sharkwords/static/js/sharkwords.js
@@ -19,13 +19,21 @@ let numWrong = 0;
 // Loop over the chars in `word` and create divs.
 //
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  // Creates a blank line for each letter of the word
+
+  for (const letter of WORDS) {
+    $('#word-container').append(`<div class="letter-box ${letter}"></div>`);
+  }
 };
 
 // Loop over each letter in `ALPHABET` and generate buttons.
 //
 const generateLetterButtons = () => {
-  // Replace this with your code
+  // creates a box for each letter of the alphabet 
+
+  for (const letter of ALPHABET){
+    $('#letter-buttons').append(`<button>${letter}</button>`);
+  }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
